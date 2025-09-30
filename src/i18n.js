6dda732f310d6991762fb157ca44655600a13c3a
Darelle <gochuicodarelleq@gmail.com>
2025-09-30 15:26:37 +0800

    ShipX v1.6.2

--- src/i18n.js
+++ src/i18n.js
@@ -11,7 +11,7 @@ i18n.use(initReactI18next).init({
     vn: { translation: vn },
     my: { translation: my },
   },
-  lng: localStorage.getItem("lang") || "my",
+  lng: localStorage.getItem("lang") || "en",
   fallbackLng: "en",
   interpolation: { escapeValue: false },
 });
