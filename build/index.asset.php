56a4c2a2e73cb07a3a0fc595eabd58f86bcece54
Darelle <gochuicodarelleq@gmail.com>
2025-10-07 20:01:00 +0800

    ShipX v1.8.6

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '572de6d6d77fbdcac477');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '20aaa192e0bfbb27e1bb');
