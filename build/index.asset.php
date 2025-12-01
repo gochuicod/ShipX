53ec703b0e74308ac127f0363852742c7bf492d5
Darelle <gochuicodarelleq@gmail.com>
2025-12-01 12:44:57 +0800

    ShipX v1.25.6.2
    Hotfix for shipment tracker blank page errors.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '10934fd510064192c053');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => 'caa4bcf004538b5bc75f');
