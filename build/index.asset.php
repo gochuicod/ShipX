0bf42dddf771012fe0e4959bcc37fee1da570b23
Darelle <gochuicodarelleq@gmail.com>
2026-01-09 01:22:33 +0800

    ShipX v2.22.9
    fixed platform section id.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '4623bad45106463e2db3');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '8254b5d5d61503d342d8');
