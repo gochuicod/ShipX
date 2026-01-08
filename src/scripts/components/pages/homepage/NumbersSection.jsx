42f7a445a6b3eb8f1027a56a7e0c7becf910ad4c
Darelle <gochuicodarelleq@gmail.com>
2026-01-09 01:29:09 +0800

    ShipX v2.22.9
    added cta for numbers section

--- src/scripts/components/pages/homepage/NumbersSection.jsx
+++ src/scripts/components/pages/homepage/NumbersSection.jsx
@@ -1,10 +1,13 @@
 import StatItems from "../../library/StatItems";
 import Container from "../../library/Container";
+import HighlightedHeading from "../../library/HighlightedHeading";
+import AppButton from "../../library/AppButton";
 
 import { Badge } from "../../../../styles/badge";
-import HighlightedHeading from "../../library/HighlightedHeading";
 import { getNumbersSectionStatsData } from "../../../utils/constants";
+
 import { useTranslation } from "react-i18next";
+import { CalendarDays } from "lucide-react";
 
 export default function NumbersSection() {
   const { t } = useTranslation();
@@ -33,6 +36,7 @@ export default function NumbersSection() {
               flex flex-wrap 2xl:flex-nowrap
               justify-center items-start
               mt-10 2xl:mt-14
+              mb-10.5
               p-[24px_0px]
               gap-y-10
               gap-x-2 sm:gap-x-4 md:gap-x-12 2xl:gap-8
@@ -50,6 +54,13 @@ export default function NumbersSection() {
           />
         ))}
       </div>
+
+      <AppButton
+        to="/book-a-demo"
+        text={t("header.book_a_demo")}
+        withLeftIcon={true}
+        leftIcon={<CalendarDays className="size-5" />}
+      />
     </Container>
   );
 }
