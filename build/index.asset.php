5dfb42d6158c4d0dd484789deb522cc7da573318
Darelle <gochuicodarelleq@gmail.com>
2026-01-21 02:20:30 +0800

    build(system build): updated build files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => 'd94da4c6880767626aac');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '61735c19b4cf92616d04');
